a4eeb52bc30a7c1f9087edada7ad4704e462be6a
Daniel Tabion <dctabion@gmail.com>
2015-12-02 11:50:03 -0600

    app completed

--- controllers/task.rb
+++ controllers/task.rb
@@ -4,6 +4,7 @@ class TaskController < ApplicationController
   get '/' do
     # 1. get a list of all task models
     # and set it to the @tasks variable
+    @tasks = Task.all
 
     # return view
     erb :read
@@ -19,12 +20,16 @@ class TaskController < ApplicationController
     # 2. Check the `params` hash and
     # the create view. Notice the keys
     # being passed in along with the values.
+
     # Take these values and create a *new*
     # instance of your Task model.
     # Assign it to a variable called @task
     # update the attributes withn the values
     # from params. Then save it!
-
+    @task = Task.new
+    @task.name = param[:name]
+    @task.description = param[:desc]
+    @task.save
     # return view
     @message = 'You task was added!'
     erb :message
@@ -39,9 +44,11 @@ class TaskController < ApplicationController
     # Assign it the @task instance variable.
     # This view will not render until the code works.
 
+     @task = Task.find(params[:id])
     # return view
     erb :update
   end
+
   post '/update' do
     # 4. Just like create, we need to find our model.
     # Then update the attributes and save it!
@@ -49,7 +56,10 @@ class TaskController < ApplicationController
     # Shhh... we've used a *hidden* input on the :create
     # view to hold the id. This is so users can't try
     # to edit it! Sneaky, sneaky.
-
+    @task = Task.find(params[:id])
+    @task.name = params[:name]
+    @task.description = params[:desc]
+    @task.save
     # return view
     @message = 'You task was updated!'
     erb :message
@@ -62,7 +72,7 @@ class TaskController < ApplicationController
     # Since we know the `id` of the model, we
     # should have no problem finding it?
     # We need it for our view.
-
+    @task = Task.find(params[:id])
     # return view
     erb :destroy
   end
@@ -73,7 +83,7 @@ class TaskController < ApplicationController
     # should have no problem finding it?
     # Then, we just need to remove it from
     # our database table.
-
+    Task.find(params[:id]).destroy
     # return view
     @message = 'You task was destroyed! BUAHAHAHA'
     erb :message
